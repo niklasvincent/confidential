f9e6f634ec0d5b77d6a0685d683fe709e95796aa
Niklas Lindblad <niklas@lindblad.info>
2017-12-03 23:21:33 +0000

    Remove unused import

--- commands/exec_test.go
+++ commands/exec_test.go
@@ -3,7 +3,6 @@ package commands
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
-	"github.com/nlindblad/confidential/environment"
 )
 
 func Test_extractCommandWithFlags(t *testing.T) {
